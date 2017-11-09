32572ee6e1ec238d597be55dcece9c23b1cba7bf
Rhicheek Patra <patra@lpdxeon2680.epfl.ch>
2017-11-09 16:38:35 +0100

    Removed most of the functions except the basic ones to be ported to C++
    temp

--- include/model.py
+++ include/model.py
@@ -13,35 +13,6 @@ def model():
         y = tf.placeholder(tf.float32, shape=[None, _NUM_CLASSES], name='Output')
         x_image = tf.reshape(x, [-1, _IMAGE_SIZE, _IMAGE_SIZE, _IMAGE_CHANNELS], name='images')
 
-    def _variable_with_weight_decay(name, shape, stddev, wd):
-        """Helper to create an initialized Variable with weight decay.
-        Note that the Variable is initialized with a truncated normal distribution.
-        A weight decay is added only if one is specified.
-        Args:
-          name: name of the variable
-          shape: list of ints
-          stddev: standard deviation of a truncated Gaussian
-          wd: add L2Loss weight decay multiplied by this float. If None, weight
-              decay is not added for this Variable.
-        Returns:
-          Variable Tensor
-        """
-        dtype = tf.float32
-        var = _variable_on_cpu(
-            name,
-            shape,
-            tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
-        if wd is not None:
-            weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
-            tf.add_to_collection('losses', weight_decay)
-        return var
-
-    def _variable_on_cpu(name, shape, initializer):
-        with tf.device('/cpu:0'):
-            dtype = tf.float32
-            var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
-        return var
-
     def put_kernels_on_grid(kernel, pad=1):
 
         '''Visualize conv. features as an image (mostly for the 1st layer).
@@ -99,82 +70,89 @@ def model():
         return x7
 
     with tf.variable_scope('conv1') as scope:
-        kernel = _variable_with_weight_decay('weights', shape=[5, 5, 3, 64], stddev=5e-2, wd=0.0)
+        kernel = tf.Variable(tf.truncated_normal([5, 5, 3, 64], dtype=tf.float32, stddev=5e-2), name='weights')
         conv = tf.nn.conv2d(x_image, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
-        pre_activation = tf.nn.bias_add(conv, biases)
-        conv1 = tf.nn.relu(pre_activation, name=scope.name)
+        biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(bias, name=scope.name)
+
     with tf.variable_scope('Visualization'):
         grid = put_kernels_on_grid(kernel)
         tf.summary.image('conv1/filters', grid, max_outputs=1)
     tf.summary.histogram('Convolution_layers/conv1', conv1)
     tf.summary.scalar('Convolution_layers/conv1', tf.nn.zero_fraction(conv1))
 
-    norm1 = tf.nn.lrn(conv1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm1')
-    pool1 = tf.nn.max_pool(norm1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
 
     with tf.variable_scope('conv2') as scope:
-        kernel = _variable_with_weight_decay('weights', shape=[5, 5, 64, 64], stddev=5e-2, wd=0.0)
+        kernel = tf.Variable(tf.truncated_normal([5, 5, 64, 64], dtype=tf.float32, stddev=5e-2), name='weights')
         conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
-        pre_activation = tf.nn.bias_add(conv, biases)
-        conv2 = tf.nn.relu(pre_activation, name=scope.name)
+        biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope.name)
+
     tf.summary.histogram('Convolution_layers/conv2', conv2)
     tf.summary.scalar('Convolution_layers/conv2', tf.nn.zero_fraction(conv2))
 
-    norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm2')
-    pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool2')
 
     with tf.variable_scope('conv3') as scope:
-        kernel = _variable_with_weight_decay('weights', shape=[3, 3, 64, 128], stddev=5e-2, wd=0.0)
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 64, 128], dtype=tf.float32, stddev=5e-2), name='weights')
         conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.0))
-        pre_activation = tf.nn.bias_add(conv, biases)
-        conv3 = tf.nn.relu(pre_activation, name=scope.name)
+        biases = tf.Variable(tf.constant(0.0, shape=[128], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv3 = tf.nn.relu(bias, name=scope.name)
+
     tf.summary.histogram('Convolution_layers/conv3', conv3)
     tf.summary.scalar('Convolution_layers/conv3', tf.nn.zero_fraction(conv3))
 
     with tf.variable_scope('conv4') as scope:
-        kernel = _variable_with_weight_decay('weights', shape=[3, 3, 128, 128], stddev=5e-2, wd=0.0)
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 128, 128], dtype=tf.float32, stddev=5e-2), name='weights')
         conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.0))
-        pre_activation = tf.nn.bias_add(conv, biases)
-        conv4 = tf.nn.relu(pre_activation, name=scope.name)
+        biases = tf.Variable(tf.constant(0.0, shape=[128], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv4 = tf.nn.relu(bias, name=scope.name)
     tf.summary.histogram('Convolution_layers/conv4', conv4)
     tf.summary.scalar('Convolution_layers/conv4', tf.nn.zero_fraction(conv4))
 
     with tf.variable_scope('conv5') as scope:
-        kernel = _variable_with_weight_decay('weights', shape=[3, 3, 128, 128], stddev=5e-2, wd=0.0)
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 128, 128], dtype=tf.float32, stddev=5e-2), name='weights')
         conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding='SAME')
-        biases = _variable_on_cpu('biases', [128], tf.constant_initializer(0.0))
-        pre_activation = tf.nn.bias_add(conv, biases)
-        conv5 = tf.nn.relu(pre_activation, name=scope.name)
+        biases = tf.Variable(tf.constant(0.0, shape=[128], dtype=tf.float32), trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv5 = tf.nn.relu(bias, name=scope.name)
+
     tf.summary.histogram('Convolution_layers/conv5', conv5)
     tf.summary.scalar('Convolution_layers/conv5', tf.nn.zero_fraction(conv5))
 
-    norm3 = tf.nn.lrn(conv5, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm3')
-    pool3 = tf.nn.max_pool(norm3, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool3')
+    pool3 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool3')
 
     with tf.variable_scope('fully_connected1') as scope:
         reshape = tf.reshape(pool3, [-1, _RESHAPE_SIZE])
         dim = reshape.get_shape()[1].value
-        weights = _variable_with_weight_decay('weights', shape=[dim, 384], stddev=0.04, wd=0.004)
-        biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+        weights = tf.Variable(tf.truncated_normal([dim, 384], dtype=tf.float32, stddev=0.04), name='weights')
+        biases = tf.Variable(tf.constant(0.1, shape=[384], dtype=tf.float32), trainable=True, name='biases')
         local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+
     tf.summary.histogram('Fully connected layers/fc1', local3)
     tf.summary.scalar('Fully connected layers/fc1', tf.nn.zero_fraction(local3))
 
     with tf.variable_scope('fully_connected2') as scope:
-        weights = _variable_with_weight_decay('weights', shape=[384, 192], stddev=0.04, wd=0.004)
-        biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+        weights = tf.Variable(tf.truncated_normal([384, 192], dtype=tf.float32, stddev=0.04), name='weights')
+        biases = tf.Variable(tf.constant(0.1, shape=[192], dtype=tf.float32), trainable=True, name='biases')
         local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+
+
+
     tf.summary.histogram('Fully connected layers/fc2', local4)
     tf.summary.scalar('Fully connected layers/fc2', tf.nn.zero_fraction(local4))
 
     with tf.variable_scope('output') as scope:
-        weights = _variable_with_weight_decay('weights', [192, _NUM_CLASSES], stddev=1 / 192.0, wd=0.0)
-        biases = _variable_on_cpu('biases', [_NUM_CLASSES], tf.constant_initializer(0.0))
+        weights = tf.Variable(tf.truncated_normal([192, _NUM_CLASSES], dtype=tf.float32, stddev=1 / 192.0), name='weights')
+        biases = tf.Variable(tf.constant(0.0, shape=[_NUM_CLASSES], dtype=tf.float32), trainable=True, name='biases')
         softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+
+
     tf.summary.histogram('Fully connected layers/output', softmax_linear)
 
     global_step = tf.Variable(initial_value=0, name='global_step', trainable=False)
